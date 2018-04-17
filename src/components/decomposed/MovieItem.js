d8dfce23fff0bab8f0b72479b432024bd2dfca73
Greg Douglas <gwshark0@gmail.com>
2018-04-17 10:41:17 -0400

    Make MovieItem more generic

--- src/components/decomposed/MovieItem.js
+++ src/components/decomposed/MovieItem.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import Favorite from './Favorite';
 
 import './MovieItem.css';
 
 function MovieItem(props) {
+  const {
+    className,
+    tag: Tag,
+    ...rest,
+  } = props;
+
+  const classes = classNames(className, 'movie-item');
+
   return (
-    <div className="movie-item" onClick={props.onClick}>
+    <div {...rest} className={classes}>
       <Favorite active={props.favorite} />
       <img
         className="movie-item__poster"
@@ -24,7 +33,13 @@ MovieItem.propTypes = {
   favorite: PropTypes.bool,
   posterPath: PropTypes.string,
   title: PropTypes.string,
+  className: PropTypes.string,
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
   onClick: PropTypes.func,
+}
+
+MovieItem.defaultProps = {
+  tag: 'div',
 }
 
 export default MovieItem;
